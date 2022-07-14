7c6a398f9d7af5b9b213aae546ce89a32163ab0d
nursroman <nursultanroman@gmail.com>
2022-07-14 22:22:56 +0600

    lesson 6.1

---
+++ hw1_lesson6.py
@@ -0,0 +1,18 @@
+from time import sleep
+class TrafficLight:
+    __color = ['Красный', 'Желтый', 'Зеленый']
+    def running(self):
+        i = 0
+        while i < 3:
+            print(f'Светофор переключается , {TrafficLight.__color[i]}')
+            if i == 0:
+                sleep(7)
+            elif i == 1:
+                sleep(5)
+            elif i == 2:
+                sleep(3)
+            i += 1
+
+
+TrafficLight = TrafficLight()
+TrafficLight.running()
